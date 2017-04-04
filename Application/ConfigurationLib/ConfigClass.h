4335a11f199c745509a1566626501e21f4ec698b
AJIOB <aleksandr9809@gmail.com>
2017-04-04 09:20:39 +0300

    App: Working with configs are added

---
+++ Application/ConfigurationLib/ConfigClass.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "pch.h"
+
+namespace AJIOB
+{
+	class ConfigClass;
+}
+
+class AJIOB::ConfigClass
+{
+	const std::string cl_fileName;
+	static const std::string cl_folder;// = "config\\";
+	static const std::string cl_extension;// = ".ini";
+
+	std::map<std::string, std::string> cl_configData;
+
+	bool LoadData();
+
+	//block for copying class
+	ConfigClass(const ConfigClass&) {}
+
+public:
+
+	ConfigClass(const std::string& fileName = "default");
+
+	std::string GetFileName() const;
+
+	//return value by key. If cannot find key, return empty string
+	std::string Get(const std::string& key) const;
+	std::string operator[] (const std::string& key) const;
+};
